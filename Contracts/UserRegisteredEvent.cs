f2be4f710a32cb19367b46857345e2fb076ebdb4
Jordy Moreno Arias <yordimorenoarias.11@gmail.com>
2026-05-05 19:29:26 -0400

    Add UserController and UserRegisteredEvent
    Introduce a new API controller (UserController) exposing GET api/user/{id} to return user profiles via IUserService. Add Contracts.UserRegisteredEvent model (Id, FirstName, LastName) for event data. Clean up the project file by removing the explicit Controllers folder entry from UserService.API.csproj.

---
+++ Contracts/UserRegisteredEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace Contracts
+{
+    internal class UserRegisteredEvent
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
---
+++ UserService.API/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UserService.Application.Interfaces.Services;
+
+namespace UserService.API.Controllers
+{
+    /// <summary>
+    /// Controller for managing user profiles and related operations.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        /// <summary>
+        /// Get user profile by user ID.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> User profile information.</returns>
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserById(Guid id)
+        {
+            var user = await _userService.GetUserProfileByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+    }
+}
